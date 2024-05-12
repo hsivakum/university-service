af1399fe59d55c4f60434b2d9add7de48f5d0624
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2024-05-11 23:39:02 -0400

    add simple db connection code

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=SYS password=password database=SYS sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
